b7b08a1d21cf0a0e1eb31cff12e117cf4045a1ef
Vakil Verma <vakilverma7@gmail.com>
2025-06-17 22:05:50 +0530

    get user api

--- src/app.js
+++ src/app.js
@@ -3,6 +3,27 @@ const { connectDB } = require("./config/database");
 const User = require("./models/user");
 const app = express();
 app.use(express.json());
+app.get("/user", async (req, res) => {
+  // const userEmail = req.body.emailId;
+  try {
+    const user = await User.findOne();
+    if (!user) {
+      res.status(404).json("user not found!");
+    } else {
+      res.status(200).json(user);
+    }
+  } catch (err) {
+    res.status(400).json("Something went wrong");
+  }
+});
+app.get("/feed", async (req, res, next) => {
+  try {
+    const users = await User.find({});
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).send("Something went wrong");
+  }
+});
 app.post("/signup", async (req, res) => {
   //creating a new instance of the user model
   const userInstance = new User(req.body);
