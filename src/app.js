842a4c2ce6dffc3b1db8c9effd38361e00ed71db
Vakil Verma <vakilverma7@gmail.com>
2025-06-23 21:34:16 +0530

    schema updates

--- src/app.js
+++ src/app.js
@@ -8,18 +8,18 @@ app.get("/user", async (req, res) => {
   try {
     const user = await User.findOne();
     if (!user) {
-      res.status(404).json("user not found!");
+      res.status(404).send("user not found!");
     } else {
-      res.status(200).json(user);
+      res.status(200).send(user);
     }
   } catch (err) {
-    res.status(400).json("Something went wrong");
+    res.status(400).send("Something went wrong");
   }
 });
 app.get("/feed", async (req, res, next) => {
   try {
     const users = await User.find({});
-    res.status(200).json(users);
+    res.status(200).send(users);
   } catch (err) {
     res.status(500).send("Something went wrong");
   }
@@ -31,7 +31,48 @@ app.post("/signup", async (req, res) => {
     await userInstance.save();
     res.send("User created successfully!");
   } catch (err) {
+    console.log(err);
     res.status(400).send("Error while saving the database!");
+  }
+});
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    await User.findByIdAndDelete(userId);
+    res.status(200).send("User deleted successfully!");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
+  const data = req.body;
+  try {
+    const UPDATE_FIELDS = [
+      "firstName",
+      "lastName",
+      "gendar",
+      "about",
+      "skills",
+      "photoUrl",
+    ];
+    const isUpdatedAllowed = Object.keys(data).every((k) =>
+      UPDATE_FIELDS.includes(k)
+    );
+    if (!isUpdatedAllowed) {
+      throw new Error("Updates are not allowed!");
+    }
+    if (data?.skills?.length > 10) {
+      throw new Error("Skills can't be more than 10");
+    }
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "after",
+      runValidators: true,
+    });
+    res.status(200).send("user updated Sucessfully!");
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Something went wrong!", err.message);
   }
 });
 connectDB()
--- src/models/user.js
+++ src/models/user.js
@@ -1,22 +1,73 @@
 const mongoose = require("mongoose");
-let userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const validator = require("validator");
+let userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      lowercase: true,
+      minLength: 4,
+      maxLength: 50,
+    },
+    lastName: {
+      type: String,
+      maxLength: 50,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+      trim: true,
+      validate(value) {
+        console.log(value);
+        if (!validator.isEmail(value)) {
+          throw new Error("Email is not valid!");
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Password is not strong enough!");
+        }
+      },
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gendar: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender is not valid!");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://skiblue.co.uk/wp-content/uploads/2015/06/dummy-profile.png",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Photo URL is not valid!");
+        }
+      },
+    },
+    about: {
+      type: String,
+      default: "This is default user!",
+      maxLength: 1500,
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gendar: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 module.exports = mongoose.model("User", userSchema);
